9108837554c33aa8b6aacce06cd8427391b2b92b
longhiisa <neveslonghiisabela@gmail.com>
2025-06-03 17:01:31 -0300

    3.6

--- pages/areas/script.js
+++ pages/areas/script.js
@@ -1,18 +1,33 @@
-document.addEventListener('DOMContentLoaded', async function() {
-const area = "Mistieri";
-const token = localStorage.getItem('token');
-const response = await fetch(`http://localhost:3000/brinquedos/area${area}`, {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-         authorization: "Bearer "  +  token
-    },
+document.addEventListener('DOMContentLoaded', async function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const area = urlParams.get('area'); // corrigido aqui
+
+    const token = String(localStorage.getItem('token').replaceAll('"', ''));
+
+    const response = await fetch(`http://localhost:3000/brinquedos/area/${area}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            authorization: "Bearer " + token
+        },
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    const grid = document.querySelector(".rides-grid");
+    grid.innerHTML = ""; // Clear previous content
+
+    for (const brinquedo of data.Resultados) {
+        console.log(brinquedo);
+
+        grid.innerHTML += `<div class="ride-card">
+            <div class="ride-image" style="background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUhZ_Pjz20DN5fzXsUEaAufsOt0DP_r_raqw&s')"></div>
+            <div class="ride-info">
+                <h3 class="ride-name">${brinquedo.name}</h3>
+                <div class="ride-time">${brinquedo.waiting_time}</div>
+                <span class="ride-status status-busy">${brinquedo.status}</span>
+            </div>
+        </div>`;
+    }
 });
-
-const data = await response.json();
-console.log(data);
-
-
-});
-
-
